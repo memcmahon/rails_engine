2ff44464b0bdffa5943bbe2a5be36f2447d02eb6
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-13 11:29:15 -0600

    Updates date query params

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -5,9 +5,9 @@ class Api::V1::Merchants::SearchController < ApplicationController
     elsif params[:name]
       render json: Merchant.find_all_by_name('name', params[:name])
     elsif params[:created_at]
-      render json: Merchant.where(merchant_params)
+      render json: Merchant.where(created_at: params[:created_at])
     elsif params[:updated_at]
-      render json: Merchant.where(merchant_params)
+      render json: Merchant.where(updated_at: params[:updated_at])
     end
   end
 
@@ -17,14 +17,9 @@ class Api::V1::Merchants::SearchController < ApplicationController
     elsif params[:name]
       render json: Merchant.find_by_name('name', params[:name])
     elsif params[:created_at]
-      render json: Merchant.find_by(merchant_params)
+      render json: Merchant.find_by(created_at: params[:created_at])
     elsif params[:updated_at]
-      render json: Merchant.find_by(merchant_params)
+      render json: Merchant.find_by(updated_at: params[:updated_at])
     end
   end
-
-  private
-    def merchant_params
-      params.permit(:created_at, :updated_at)
-    end
 end
