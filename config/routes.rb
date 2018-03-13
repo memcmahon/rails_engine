a8b3de28706863bdd079900ca46a9cf28c1e7fb1
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-12 19:28:11 -0600

    Adds routes for find merchants

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      namespace :merchants do
+        resources :find, only: [:index], to: "search#show"
+      end
       resources :merchants, only: [:index, :show]
     end
   end
