80a23894a9f7a26e67ba315aa94ef19d8453b30d
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-13 13:55:59 -0600

    prep for rebase

--- spec/requests/api/v1/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions_request_spec.rb
@@ -106,4 +106,88 @@ describe 'Transactions API' do
     transaction = JSON.parse(response.body)
     expect(transaction["id"]).to eq(transaction_4.id)
   end
+
+  xit 'finds all transactions by id parameter' do
+    transaction_1, transaction_2, transaction_3 = create_list(:transaction, 3)
+
+    get "/api/v1/transactions/find_all?id=#{transaction_2.id}"
+
+    expect(response).to be_success
+
+    transactions = JSON.parse(response.body)
+    expect(transactions).to be_instance_of(Array)
+    expect(transaction[0]["id"]).to eq(transaction_2.id)
+  end
+
+  xit 'finds one transaction by invoice_id parameter' do
+    transaction_1, transaction_2, transaction_3 = create_list(:transaction, 3)
+
+    get "/api/v1/transactions/find_all?invoice_id=#{transaction_2.invoice_id}"
+
+    expect(response).to be_success
+
+    transactions = JSON.parse(response.body)
+    expect(transactions.count).to eq(3)
+  end
+
+  xit 'finds one transaction by credit_card_number parameter' do
+    transaction_1, transaction_2, transaction_3 = create_list(:transaction, 3)
+
+    get "/api/v1/transactions/find?credit_card_number=#{transaction_2.credit_card_number}"
+
+    expect(response).to be_success
+
+    transaction = JSON.parse(response.body)
+    expect(transaction["id"]).to eq(transaction_2.id)
+  end
+
+  xit 'finds one transaction by result parameter' do
+    transaction_1, transaction_2, transaction_3 = create_list(:transaction, 3, result: 'failed')
+    transaction_4 = create(:transaction)
+
+    get "/api/v1/transactions/find?result=#{transaction_4.result}"
+
+    expect(response).to be_success
+
+    transaction = JSON.parse(response.body)
+    expect(transaction["id"]).to eq(transaction_4.id)
+  end
+
+  xit 'finds one transaction by case insensitive result parameter' do
+    transaction_1, transaction_2, transaction_3 = create_list(:transaction, 3, result: 'failed')
+    transaction_4 = create(:transaction)
+
+    get "/api/v1/transactions/find?result=SUCcESs"
+
+    expect(response).to be_success
+
+    transaction = JSON.parse(response.body)
+    expect(transaction["id"]).to eq(transaction_4.id)
+  end
+
+  xit 'finds one transaction by created_at parameter' do
+    created_at = "2017-03-14 12:12:12 UTC"
+    transaction_1, transaction_2, transaction_3 = create_list(:transaction, 3)
+    transaction_4 = create(:transaction, created_at: created_at)
+
+    get "/api/v1/transactions/find?created_at=#{created_at}"
+
+    expect(response).to be_success
+
+    transaction = JSON.parse(response.body)
+    expect(transaction["id"]).to eq(transaction_4.id)
+  end
+
+  xit 'finds one transaction by updated_at parameter' do
+    updated_at = "2017-03-14 12:12:12 UTC"
+    transaction_1, transaction_2, transaction_3 = create_list(:transaction, 3)
+    transaction_4 = create(:transaction, updated_at: updated_at)
+
+    get "/api/v1/transactions/find?updated_at=#{updated_at}"
+
+    expect(response).to be_success
+
+    transaction = JSON.parse(response.body)
+    expect(transaction["id"]).to eq(transaction_4.id)
+  end
 end
