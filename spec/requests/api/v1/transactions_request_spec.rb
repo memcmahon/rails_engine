212575464214a5a331e9353b5d8195ce851b7526
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-13 13:28:58 -0600

    Adds spec for transaction show

--- spec/requests/api/v1/transactions_request_spec.rb
+++ spec/requests/api/v1/transactions_request_spec.rb
@@ -11,4 +11,16 @@ describe 'Transactions API' do
     merchants = JSON.parse(response.body)
     expect(merchants.count).to eq(5)
   end
+
+  it 'sends one transaction by id' do
+    create_list(:transaction, 5)
+    id = create(:transaction).id
+
+    get "/api/v1/transactions/#{id}.json"
+
+    expect(response).to be_success
+
+    transaction = JSON.parse(response.body)
+    expect(transaction["id"]).to eq(id)
+  end
 end
