e7fbf26a0ba2d37bf0b790ec70feb318546644c8
Ellen Cornelius <ecorneli@iwu.edu>
2018-03-13 13:36:08 -0600

    Add validations and relationships testing to invoices.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,9 @@
 class Invoice < ApplicationRecord
+  validates_presence_of :customer_id, :merchant_id, :status
   belongs_to :customer
   belongs_to :merchant
+
+  def self.random
+    order("RANDOM()").first
+  end
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe Invoice, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Invoice, type: :model do
+  it { should validate_presence_of(:customer_id) }
+  it { should validate_presence_of(:merchant_id) }
+  it { should validate_presence_of(:status) }
+
+  describe "relationships" do
+    it { should belong_to(:customer) }
+    it { should belong_to(:merchant) }
+  end
 end
