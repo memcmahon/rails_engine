b010ff9401d7ded390ae7bf265a52fe01e3cbf86
Ellen Cornelius <ecorneli@iwu.edu>
2018-03-13 14:45:01 -0600

    Update items factory.

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :item do
-    
+    name "Lego"
+    description "Your kid will want to be an architect after playing with these"
+    unit_price 10000
+    merchant
   end
 end
