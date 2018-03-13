b083b3e06477dde450a7cbad295fe254ade0c759
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-13 12:57:51 -0600

    Implements random merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,4 +6,8 @@ class Merchant < ApplicationRecord
 
   scope :find_by_name, lambda { |name, value| where("lower(#{name}) = ?", value.downcase.gsub("_", " ")).first }
   scope :find_all_by_name, lambda { |name, value| where("lower(#{name}) = ?", value.downcase.gsub("_", " ")) }
+
+  def self.random_merchant
+    all.sample(1)[0]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :merchants do
-        resources :find, to: "search#show"
-        resources :find_all, to: "search#index"
+        get '/find', to: "search#show"
+        get '/find_all', to: "search#index"
+        get '/random', to: "random#show"
       end
       get '/merchants', to: "merchants#index"
       get '/merchants/:id', to:"merchants#show"
