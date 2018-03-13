e3bb1465dd911a0cefa12c060ccb426f4665f8cd
Ellen Cornelius <ecorneli@iwu.edu>
2018-03-13 14:27:00 -0600

    Change item/items local vairbale to invoice/invoices.

--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -10,77 +10,77 @@ describe "Invoices API" do
   it "sends a list of all invoices" do
     get "/api/v1/invoices.json"
 
-    items = JSON.parse(response.body)
+    invoices = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(items.count).to eq(3)
+    expect(invoices.count).to eq(3)
   end
 
   it "sends one invoice" do
     get "/api/v1/invoices/#{@invoice_1.id}.json"
 
-    item = JSON.parse(response.body)
+    invoice = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(item["id"]).to eq(@invoice_1.id)
+    expect(invoice["id"]).to eq(@invoice_1.id)
   end
 
   it "sends one invoice based on id" do
     get "/api/v1/invoices/find?id=#{@invoice_1.id}"
 
-    item = JSON.parse(response.body)
+    invoice = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(item["id"]).to eq(@invoice_1.id)
+    expect(invoice["id"]).to eq(@invoice_1.id)
   end
 
   it "sends one invoice based on customer id" do
     get "/api/v1/invoices/find?customer_id=#{@invoice_1.customer_id}"
 
-    item = JSON.parse(response.body)
+    invoice = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(item["customer_id"]).to eq(@invoice_1.customer_id)
+    expect(invoice["customer_id"]).to eq(@invoice_1.customer_id)
   end
 
   it "sends one invoice based on merchant id" do
     get "/api/v1/invoices/find?merchant_id=#{@invoice_1.merchant_id}"
 
-    item = JSON.parse(response.body)
+    invoice = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(item["merchant_id"]).to eq(@invoice_1.merchant_id)
+    expect(invoice["merchant_id"]).to eq(@invoice_1.merchant_id)
   end
 
   it "sends one invoice based on status" do
     get "/api/v1/invoices/find?status=#{@invoice_2.status}"
 
-    item = JSON.parse(response.body)
+    invoice = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(item["status"]).to eq("pending")
+    expect(invoice["status"]).to eq("pending")
   end
 
   it "sends one invoice based on created_at" do
     date = "2017-03-12 14:53:59 UTC"
-    invoice = create(:invoice, created_at: date)
+    invoice_1 = create(:invoice, created_at: date)
     get "/api/v1/invoices/find?created_at=#{date}"
 
-    item = JSON.parse(response.body)
+    invoice = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(item["id"]).to eq(invoice.id)
+    expect(invoice["id"]).to eq(invoice_1.id)
   end
 
   it "sends one invoice based on updated_at" do
     date = "2017-03-12 14:53:59 UTC"
-    invoice = create(:invoice, updated_at: date)
+    invoice_1 = create(:invoice, updated_at: date)
     get "/api/v1/invoices/find?updated_at=#{date}"
 
-    item = JSON.parse(response.body)
+    invoice = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(item["id"]).to eq(invoice.id)
+    expect(invoice["id"]).to eq(invoice_1.id)
   end
 
   it "renders 404 if key does not exist" do
@@ -92,10 +92,10 @@ describe "Invoices API" do
   it "sends all invoices based on id" do
     get "/api/v1/invoices/find_all?id=#{@invoice_1.id}"
 
-    items = JSON.parse(response.body)
+    invoices = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(items[0]["id"]).to eq(@invoice_1.id)
+    expect(invoices[0]["id"]).to eq(@invoice_1.id)
   end
 
   it "sends all invoices based on customer id" do
@@ -105,10 +105,10 @@ describe "Invoices API" do
     invoice_6 = create(:invoice, customer_id: customer.id)
     get "/api/v1/invoices/find_all?customer_id=#{invoice_4.customer_id}"
 
-    items = JSON.parse(response.body)
+    invoices = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(items.count).to eq(3)
+    expect(invoices.count).to eq(3)
   end
 
   it "sends all invoices based on merchant id" do
@@ -118,19 +118,19 @@ describe "Invoices API" do
     invoice_6 = create(:invoice, merchant_id: merchant.id)
     get "/api/v1/invoices/find_all?merchant_id=#{invoice_4.merchant_id}"
 
-    items = JSON.parse(response.body)
+    invoices = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(items.count).to eq(3)
+    expect(invoices.count).to eq(3)
   end
 
   it "sends all invoices based on status" do
     get "/api/v1/invoices/find_all?status=shipped"
 
-    items = JSON.parse(response.body)
+    invoices = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(items.count).to eq(2)
+    expect(invoices.count).to eq(2)
   end
 
   it "sends all invoices based on created_at" do
@@ -139,10 +139,10 @@ describe "Invoices API" do
     invoice_2 = create(:invoice, created_at: date)
     get "/api/v1/invoices/find_all?created_at=#{date}"
 
-    items = JSON.parse(response.body)
+    invoices = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(items.count).to eq(2)
+    expect(invoices.count).to eq(2)
   end
 
   it "sends all invoices based on updated_at" do
@@ -151,10 +151,10 @@ describe "Invoices API" do
     invoice_2 = create(:invoice, updated_at: date)
     get "/api/v1/invoices/find_all?updated_at=#{date}"
 
-    items = JSON.parse(response.body)
+    invoices = JSON.parse(response.body)
 
     expect(response).to be_success
-    expect(items.count).to eq(2)
+    expect(invoices.count).to eq(2)
   end
 
   it "renders 404 if key does not exist" do
