a0b0afd9df79ee5280b40b0a060f29b774700ebb
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-13 15:19:55 -0600

    Adds spec for find_all customers

--- spec/requests/api/v1/customers_request_spec.rb
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -91,6 +91,7 @@ describe 'Customers API' do
     customer = JSON.parse(response.body)
     expect(customer["id"]).to eq(customer_4.id)
   end
+
   it 'finds one customer by updated_at parameter' do
     updated_at = "2017-03-12 14:53:59 UTC"
     customer_1, customer_2, customer_3 = create_list(:customer, 3)
@@ -102,5 +103,110 @@ describe 'Customers API' do
 
     customer = JSON.parse(response.body)
     expect(customer["id"]).to eq(customer_4.id)
+  end
+
+  it 'finds one customer by updated_at parameter' do
+    updated_at = "2017-03-12 14:53:59 UTC"
+    customer_1, customer_2, customer_3 = create_list(:customer, 3)
+    customer_4 = create(:customer, updated_at: updated_at)
+
+    get "/api/v1/customers/find?updated_at=#{updated_at}"
+
+    expect(response).to be_success
+
+    customer = JSON.parse(response.body)
+    expect(customer["id"]).to eq(customer_4.id)
+  end
+
+  it 'finds all customers by id parameter' do
+    customer_1, customer_2, customer_3 = create_list(:customer, 3)
+
+    get "/api/v1/customers/find_all?id=#{customer_2.id}"
+
+    expect(response).to be_success
+
+    customers = JSON.parse(response.body)
+    expect(customers).to be_instance_of(Array)
+    expect(customers.count).to eq(1)
+  end
+
+  it 'finds all customers by first_name parameter' do
+    customer_1, customer_2, customer_3 = create_list(:customer, 3, first_name: "Megan")
+    customer_4 = create(:customer)
+
+    get "/api/v1/customers/find_all?first_name=#{customer_2.first_name}"
+
+    expect(response).to be_success
+
+    customers = JSON.parse(response.body)
+    expect(customers).to be_instance_of(Array)
+    expect(customers.count).to eq(3)
+  end
+
+  it 'finds all customers by case insensitive first_name parameter' do
+    customer_1, customer_2, customer_3 = create_list(:customer, 3, first_name: "Harry")
+    customer_4 = create(:customer)
+
+    get "/api/v1/customers/find_all?first_name=#{customer_2.first_name.upcase}"
+
+    expect(response).to be_success
+
+    customers = JSON.parse(response.body)
+    expect(customers).to be_instance_of(Array)
+    expect(customers.count).to eq(3)
+  end
+
+  it 'finds all customers by last_name parameter' do
+    customer_1, customer_2, customer_3 = create_list(:customer, 3, last_name: "Potter")
+    customer_4 = create(:customer)
+
+    get "/api/v1/customers/find_all?last_name=#{customer_2.last_name}"
+
+    expect(response).to be_success
+
+    customers = JSON.parse(response.body)
+    expect(customers).to be_instance_of(Array)
+    expect(customers.count).to eq(3)
+  end
+
+  it 'finds all customers by case insensitive last_name parameter' do
+    customer_1, customer_2, customer_3 = create_list(:customer, 3, last_name: "Potter")
+    customer_4 = create(:customer)
+
+    get "/api/v1/customers/find_all?last_name=#{customer_2.last_name.upcase}"
+
+    expect(response).to be_success
+
+    customers = JSON.parse(response.body)
+    expect(customers).to be_instance_of(Array)
+    expect(customers.count).to eq(3)
+  end
+
+  it 'finds all customers by created_at parameter' do
+    created_at = "2017-03-12 14:53:59 UTC"
+    customer_1, customer_2, customer_3 = create_list(:customer, 3, created_at: created_at)
+    customer_4 = create(:customer)
+
+    get "/api/v1/customers/find_all?created_at=#{created_at}"
+
+    expect(response).to be_success
+
+    customers = JSON.parse(response.body)
+    expect(customers).to be_instance_of(Array)
+    expect(customers.count).to eq(3)
+  end
+
+  it 'finds one customer by updated_at parameter' do
+    updated_at = "2017-03-12 14:53:59 UTC"
+    customer_1, customer_2, customer_3 = create_list(:customer, 3, updated_at: updated_at)
+    customer_4 = create(:customer)
+
+    get "/api/v1/customers/find_all?updated_at=#{updated_at}"
+
+    expect(response).to be_success
+
+    customers = JSON.parse(response.body)
+    expect(customers).to be_instance_of(Array)
+    expect(customers.count).to eq(3)
   end
 end
