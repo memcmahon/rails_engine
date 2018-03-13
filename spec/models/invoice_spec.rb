bbc2b4181f3e6fdc9285eaf2d9e63dccaf68e1fd
Ellen Cornelius <ecorneli@iwu.edu>
2018-03-13 15:06:28 -0600

    Add invoice_items relationship to invoices and test.

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -10,5 +10,7 @@ describe Invoice, type: :model do
   describe "relationships" do
     it { should belong_to(:customer) }
     it { should belong_to(:merchant) }
+    it { should have_many(:invoice_items)}
+    it { should have_many(:items)}
   end
 end
