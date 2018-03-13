ef09443df73da7fae84ce9aeae1bb5b8023b7f24
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-13 11:36:40 -0600

    Adds relationships to customer, transaction and merchant models

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,9 @@
 class Merchant < ApplicationRecord
   validates_presence_of :name
 
+  has_many :invoices
+  has_many :items
+
   scope :find_by_name, lambda { |name, value| where("lower(#{name}) = ?", value.downcase.gsub("_", " ")).first }
   scope :find_all_by_name, lambda { |name, value| where("lower(#{name}) = ?", value.downcase.gsub("_", " ")) }
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe Transaction, type: :model do
     it { should validate_presence_of :credit_card_number }
     it { should validate_presence_of :result }
   end
+
+  describe "Relationships" do
+    it { should belong_to :invoice }
+  end
 end
