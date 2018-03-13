4912fcb863552caca92ec7d016fdbddaa49dd790
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-13 12:12:58 -0600

    refactor merchant search to use merchant_params

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -5,9 +5,9 @@ class Api::V1::Merchants::SearchController < ApplicationController
     elsif params[:name]
       render json: Merchant.find_all_by_name('name', params[:name])
     elsif params[:created_at]
-      render json: Merchant.where(created_at: params[:created_at])
+      render json: Merchant.where(merchant_params)
     elsif params[:updated_at]
-      render json: Merchant.where(updated_at: params[:updated_at])
+      render json: Merchant.where(merchant_params)
     else
       head 404, "content-type" => "text/plain"
     end
@@ -15,13 +15,13 @@ class Api::V1::Merchants::SearchController < ApplicationController
 
   def show
     if params[:id]
-      render json: Merchant.find(params[:id])
+      render json: Merchant.find_by(merchant_params)
     elsif params[:name]
       render json: Merchant.find_by_name('name', params[:name])
     elsif params[:created_at]
-      render json: Merchant.find_by(created_at: params[:created_at])
+      render json: Merchant.find_by(merchant_params)
     elsif params[:updated_at]
-      render json: Merchant.find_by(updated_at: params[:updated_at])
+      render json: Merchant.find_by(merchant_params)
     else
       head 404, "content-type" => "text/plain"
     end
@@ -29,6 +29,6 @@ class Api::V1::Merchants::SearchController < ApplicationController
 
   private
    def merchant_params
-     params.permit(:id)
+     params.permit(:id, :created_at, :updated_at)
    end
 end
