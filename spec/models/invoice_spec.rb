e7abe82f1911be920671ff5fd3cfe7ebab104501
Ellen Cornelius <ecorneli@iwu.edu>
2018-03-13 13:45:03 -0600

    Add Validations heading.

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 describe Invoice, type: :model do
-  it { should validate_presence_of(:customer_id) }
-  it { should validate_presence_of(:merchant_id) }
-  it { should validate_presence_of(:status) }
+  describe "validations" do
+    it { should validate_presence_of(:customer_id) }
+    it { should validate_presence_of(:merchant_id) }
+    it { should validate_presence_of(:status) }
+  end
 
   describe "relationships" do
     it { should belong_to(:customer) }
