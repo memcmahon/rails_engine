285f1f8424154ba76035fc8720f8292e829af907
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-13 12:11:09 -0600

    Adds 404 response for unknown query params

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,13 +1,15 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def index
     if params[:id]
-      render json: Merchant.where(id: params[:id])
+      render json: Merchant.where(merchant_params)
     elsif params[:name]
       render json: Merchant.find_all_by_name('name', params[:name])
     elsif params[:created_at]
       render json: Merchant.where(created_at: params[:created_at])
     elsif params[:updated_at]
       render json: Merchant.where(updated_at: params[:updated_at])
+    else
+      head 404, "content-type" => "text/plain"
     end
   end
 
@@ -20,6 +22,13 @@ class Api::V1::Merchants::SearchController < ApplicationController
       render json: Merchant.find_by(created_at: params[:created_at])
     elsif params[:updated_at]
       render json: Merchant.find_by(updated_at: params[:updated_at])
+    else
+      head 404, "content-type" => "text/plain"
     end
   end
+
+  private
+   def merchant_params
+     params.permit(:id)
+   end
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -133,4 +133,18 @@ describe 'Merchants API' do
     merchants = JSON.parse(response.body)
     expect(merchants.count).to eq(3)
   end
+
+  it 'returns 404 error if find params are invalid' do
+    get "/api/v1/merchants/find?duck=goose"
+
+    expect(response).to_not be_success
+    expect(response.status).to eq(404)
+  end
+
+  it 'returns 404 error if find all params are invalid' do
+    get "/api/v1/merchants/find_all?duck=goose"
+
+    expect(response).to_not be_success
+    expect(response.status).to eq(404)
+  end
 end
