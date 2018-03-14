cedd2d0104c9e9b4a8220a19e014f8522390955b
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-14 12:02:02 -0600

    updates invoice model to avoid merge conflicts

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,9 +2,9 @@ class Invoice < ApplicationRecord
   validates_presence_of :customer_id, :merchant_id, :status
   belongs_to :customer
   belongs_to :merchant
-  has_many :transactions
   has_many :invoice_items
   has_many :items, through: :invoice_items
+  has_many :transactions
 
   scope :find_by_attribute, ->(attribute) { find_by(attribute) }
   scope :find_all_by_attribute, ->(attribute) { where(attribute) }
