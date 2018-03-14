58c1232cfd87e899603fc283aaf2600dbe6fb79f
Ellen Cornelius <ecorneli@iwu.edu>
2018-03-13 18:14:28 -0600

    Add Merchant Items Controller and show action.

---
+++ app/controllers/api/v1/merchants/merchant_items_controller.rb
@@ -0,0 +1,6 @@
+class Api::V1::Merchants::MerchantItemsController < ApplicationController
+
+  def show
+    render json: Merchant.find(params[:id]), serializer: SimpleMerchantSerializer
+  end
+end
