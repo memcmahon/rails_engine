323254df4378abfd49e3de36871bbfc76a0959fd
Ellen Cornelius <ecorneli@iwu.edu>
2018-03-13 22:04:53 -0600

    Add rank_by_revenue method to merchant.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -10,4 +10,24 @@ class Merchant < ApplicationRecord
   def self.random_record
     all.sample(1)[0]
   end
+
+  def self.rank_by_revenue(quantity)
+    select("merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue")
+    .joins(invoices: :invoice_items)
+    .joins(invoices: :transactions)
+    .where("transactions.result = 'success'")
+    .group(:id)
+    .order("revenue desc")
+  end
 end
+
+#SQL: SELECT merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue
+      #FROM merchants
+      #JOIN invoices ON invoices.merchant_id = merchants.id
+      #JOIN invoice_items ON invoice_items.invoice_id = invoices.id
+      #JOIN items ON invoice_items.item_id = items.id
+      #JOIN transactions ON transactions.invoice_id = invoices.id
+      #WHERE transaction.result = "success"
+      #GROUP BY merchants.id
+      #ORDER BY revenue
+      #LIMIT #{quantity}
