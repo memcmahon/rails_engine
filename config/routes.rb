ef0f294045541c1390e8e3b2a11b863d0bbb8210
Ellen Cornelius <ecorneli@iwu.edu>
2018-03-13 18:12:35 -0600

    Add routes for merchant items.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
         get '/find', to: "search#show"
         get '/find_all', to: "search#index"
         get '/random', to: "random#show"
+        get '/:id/items', to: "merchant_items#show"
       end
       get '/merchants', to: "merchants#index"
       get '/merchants/:id', to:"merchants#show"
