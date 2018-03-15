71e03f62bd3253c3b7c1f27c22a50ad6ec0bba24
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-15 11:40:55 -0600

    Adds spec for customer business intel

--- spec/requests/api/v1/customer_business_intel_spec.rb
+++ spec/requests/api/v1/customer_business_intel_spec.rb
@@ -5,5 +5,23 @@ describe "Customer Business Intel API" do
     customer = create(:customer)
     merchant_1, merchant_2 = create_list(:merchant, 2)
     invoice_1 = create(:invoice, customer: customer, merchant: merchant_1)
+    invoice_2 = create(:invoice, customer: customer, merchant: merchant_1)
+    invoice_3 = create(:invoice, customer: customer, merchant: merchant_2)
+    invoice_4 = create(:invoice, customer: customer, merchant: merchant_2)
+    invoice_5 = create(:invoice, customer: customer, merchant: merchant_1)
+    transaction_1 = create(:transaction, invoice: invoice_1)
+    transaction_2 = create(:transaction, invoice: invoice_2, result: 'failed')
+    transaction_3 = create(:transaction, invoice: invoice_2)
+    transaction_4 = create(:transaction, invoice: invoice_3, result: 'failed')
+    transaction_6 = create(:transaction, invoice: invoice_4)
+    transaction_7 = create(:transaction, invoice: invoice_5)
+
+    get "/api/v1/customers/#{customer.id}/favorite_merchant"
+
+    expect(response).to be_success
+
+    merchant = JSON.parse(response.body)
+
+    expect(merchant["id"]).to eq(merchant_1.id)
   end
 end
