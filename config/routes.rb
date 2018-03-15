4da9a61e4e6cf9ed48958a654ba8ab4b077e41ae
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-14 20:40:04 -0600

    Implements Invoice Customer relationship endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -37,6 +37,7 @@ Rails.application.routes.draw do
         get '/:id/transactions', to: "invoice_transactions#index"
         get '/:id/invoice_items', to: "invoice_invoice_items#index"
         get '/:id/items', to: "invoice_items#index"
+        get '/:id/customer', to: "invoice_customer#show"
       end
       get '/invoices', to: "invoices#index"
       get '/invoices/:id', to: "invoices#show"
