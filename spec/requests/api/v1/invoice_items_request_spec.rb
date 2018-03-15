5fa3a74f85abe68544c94c4cf5f0855e6a06c4ca
Ellen Cornelius <ecorneli@iwu.edu>
2018-03-15 15:13:13 -0600

    Create additional invoice items endpoints tests.

--- spec/requests/api/v1/invoice_items_request_spec.rb
+++ spec/requests/api/v1/invoice_items_request_spec.rb
@@ -15,4 +15,104 @@ describe "Items API" do
     expect(response).to be_success
     expect(invoice_items.count).to eq(3)
   end
+
+  it "sends all invoice_items" do
+    get "/api/v1/invoice_items/#{@invoice_item_1.id}.json"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item["id"]).to eq(@invoice_item_1.id)
+  end
+
+  it "sends one invoice_items with matching id" do
+    get "/api/v1/invoice_items/find?id=#{@invoice_item_1.id}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item["id"]).to eq(@invoice_item_1.id)
+  end
+
+  it "sends one invoice_item with matching item_id" do
+    get "/api/v1/invoice_items/find?item_id=#{@invoice_item_1.item_id}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item["item_id"]).to eq(@invoice_item_1.item_id)
+  end
+
+  it "sends one invoice_item with matching invoice_id" do
+    get "/api/v1/invoice_items/find?invoice_id=#{@invoice_item_1.invoice_id}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item["invoice_id"]).to eq(@invoice_item_1.invoice_id)
+  end
+
+  it "sends one invoice_item with matching quantity" do
+    get "/api/v1/invoice_items/find?quantity=#{@invoice_item_1.quantity}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item["quantity"]).to eq(@invoice_item_1.quantity)
+  end
+
+  it "sends one invoice_item with matching unit_price" do
+    get "/api/v1/invoice_items/find?unit_price=#{@invoice_item_1.unit_price}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item["unit_price"]).to eq(@invoice_item_1.unit_price)
+  end
+
+  it "sends one invoice_item with mathcing craeted_at" do
+    date = "2017-03-12 14:53:59 UTC"
+    invoice_item_4 = create(:invoice_item, created_at: date)
+    get "/api/v1/invoice_items/find?created_at=#{date}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item["id"]).to eq(invoice_item_4.id)
+  end
+
+  it "sends one invoice_item with matching updated_at" do
+    date = "2017-03-12 14:53:59 UTC"
+    invoice_item_5 = create(:invoice_item, updated_at: date)
+    get "/api/v1/invoice_items/find?updated_at=#{date}"
+
+    invoice_item = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_item["id"]).to eq(invoice_item_5.id)
+  end
+
+  it "responds with 404 if key does not exist" do
+    get "/api/v1/invoice_items/find?banana=pie"
+
+    expect(response).to_not be_success
+  end
+
+  it "sends all invoice_items with matching id" do
+    get "/api/v1/invoice_items/find_all?id=#{@invoice_item_1.id}"
+
+    invoice_items = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_items.first["id"]).to eq(@invoice_item_1.id)
+  end
+
+  it "sends all invoice_items with matching item_id" do
+    get "/api/v1/invoice_items/find_all?id=#{@invoice_item_1.id}"
+
+    invoice_items = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(invoice_items.first["id"]).to eq(@invoice_item_1.id)
+  end
 end
