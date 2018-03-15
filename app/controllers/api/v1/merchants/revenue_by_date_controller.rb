c6285d68c19e65c39f623129a6f9968334fb45c3
Ellen Cornelius <ecorneli@iwu.edu>
2018-03-14 19:25:24 -0600

    Fix json to render with total_revenue key.

--- app/controllers/api/v1/merchants/revenue_by_date_controller.rb
+++ app/controllers/api/v1/merchants/revenue_by_date_controller.rb
@@ -2,6 +2,6 @@ class Api::V1::Merchants::RevenueByDateController < ApplicationController
 
   def show
     date = params[:date].to_date
-    render json: Invoice.revenue_by_date(date)
+    render json: {total_revenue: "#{Invoice.revenue_by_date(date)}"}
   end
 end
